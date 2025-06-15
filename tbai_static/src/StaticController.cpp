ec9adfb16445884a5986922fa6758bf777d9ceab
lnotspotl <lnotspotl@gmail.com>
2025-06-15 15:39:54 -0400

    Fix static controller visualizer urdf

--- tbai_static/src/StaticController.cpp
+++ tbai_static/src/StaticController.cpp
@@ -27,11 +27,14 @@ StaticController::StaticController(const std::string &configRosParam,
       timeSinceLastVisualizationUpdate_(100.0) {
     loadSettings(configRosParam);
 
-    // Initialize robot state publisher
-    const std::string urdfFile = ros::package::getPath("ocs2_robotic_assets") + "/resources/anymal_d/urdf/anymal.urdf";
-    urdf::Model urdfModel;
+    // Setup state publisher
+    std::string urdfString;
+    if (!ros::param::get("/robot_description", urdfString)) {
+        throw std::runtime_error("Failed to get param /robot_description");
+    }
+
     KDL::Tree kdlTree;
-    kdl_parser::treeFromFile(urdfFile, kdlTree);
+    kdl_parser::treeFromString(urdfString, kdlTree);
     robotStatePublisherPtr_.reset(new robot_state_publisher::RobotStatePublisher(kdlTree));
     robotStatePublisherPtr_->publishFixedTransforms(true);
 }
