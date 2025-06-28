9957a70027434dd99404323703ca2a2883ffd74f
lnotspotl <lnotspotl@gmail.com>
2025-06-28 12:34:20 -0400

    Add mpc controller for go2

--- tbai_ros_mpc/src/MpcNode.cpp
+++ tbai_ros_mpc/src/MpcNode.cpp
@@ -5,10 +5,11 @@
 #include <ocs2_quadruped_interface/QuadrupedMpcNode.h>
 #include <ros/init.h>
 #include <tbai_core/Throws.hpp>
+#include <tbai_core/config/Config.hpp>
 
 int main(int argc, char *argv[]) {
     // Initialize ros node
-    ros::init(argc, argv, "anymal_mpc");
+    ros::init(argc, argv, "mpc_node");
     ros::NodeHandle nodeHandle;
 
     // Anymal urdf
@@ -26,27 +27,37 @@ int main(int argc, char *argv[]) {
     TBAI_THROW_UNLESS(nodeHandle.getParam("/frame_declaration_file", frameDeclarationFile),
                       "Failed to get parameter /frame_declaration_file");
 
+    std::string robotName = tbai::fromGlobalConfig<std::string>("robot_name");
+    std::unique_ptr<switched_model::QuadrupedInterface> quadrupedInterface;
+    if (robotName == "anymal_d") {
+        quadrupedInterface =
+            anymal::getAnymalInterface(urdfString, switched_model::loadQuadrupedSettings(taskSettingsFile),
+                                       anymal::frameDeclarationFromFile(frameDeclarationFile));
+    } else if (robotName == "go2") {
+        quadrupedInterface = anymal::getGo2Interface(urdfString, switched_model::loadQuadrupedSettings(taskSettingsFile),
+                                                  anymal::frameDeclarationFromFile(frameDeclarationFile));
+    } else {
+        TBAI_THROW("Robot name not supported: {}", robotName);
+    }
+
     // Prepare robot interface
-    auto anymalInterface =
-        anymal::getAnymalInterface(urdfString, switched_model::loadQuadrupedSettings(taskSettingsFile),
-                                   anymal::frameDeclarationFromFile(frameDeclarationFile));
     const auto mpcSettings = ocs2::mpc::loadSettings(taskSettingsFile);
 
-    if (anymalInterface->modelSettings().algorithm_ == switched_model::Algorithm::SQP) {
+    if (quadrupedInterface->modelSettings().algorithm_ == switched_model::Algorithm::SQP) {
         ROS_INFO_STREAM("[MpcNode] Using SQP MPC");
         std::string sqpSettingsFile;
         TBAI_THROW_UNLESS(nodeHandle.getParam("/sqp_settings_file", sqpSettingsFile),
                           "Failed to get parameter /sqp_settings_file");
         const auto sqpSettings = ocs2::sqp::loadSettings(sqpSettingsFile);
-        auto mpcPtr = switched_model::getSqpMpc(*anymalInterface, mpcSettings, sqpSettings);
-        switched_model::quadrupedMpcNode(nodeHandle, *anymalInterface, std::move(mpcPtr));
+        auto mpcPtr = switched_model::getSqpMpc(*quadrupedInterface, mpcSettings, sqpSettings);
+        switched_model::quadrupedMpcNode(nodeHandle, *quadrupedInterface, std::move(mpcPtr));
     }
 
-    if (anymalInterface->modelSettings().algorithm_ == switched_model::Algorithm::DDP) {
+    if (quadrupedInterface->modelSettings().algorithm_ == switched_model::Algorithm::DDP) {
         ROS_INFO_STREAM("[MpcNode] Using DDP MPC");
         const auto ddpSettings = ocs2::ddp::loadSettings(taskSettingsFile);
-        auto mpcPtr = switched_model::getDdpMpc(*anymalInterface, mpcSettings, ddpSettings);
-        switched_model::quadrupedMpcNode(nodeHandle, *anymalInterface, std::move(mpcPtr));
+        auto mpcPtr = switched_model::getDdpMpc(*quadrupedInterface, mpcSettings, ddpSettings);
+        switched_model::quadrupedMpcNode(nodeHandle, *quadrupedInterface, std::move(mpcPtr));
     }
 
     return EXIT_SUCCESS;
