349dfd16f98fb4e5e1bc0e2172deee7f00f94d24
lnotspotl <lnotspotl@gmail.com>
2025-09-14 10:33:55 -0400

    add autonomy examples

--- tbai_ros_safe/src/tbai_safe_node_mppi.py
+++ tbai_ros_safe/src/tbai_safe_node_mppi.py
@@ -7,10 +7,10 @@ from typing import Callable
 import matplotlib.pyplot as plt
 import numpy as np
 import rospy
-import sympy as sp
 from geometry_msgs.msg import Point, Twist
 from scipy.spatial.transform import Rotation as R
 from tbai_safe.cbf import ControlBarrierFunctionFactory, visualize_cbfs
+from tbai_safe.control import VanillaSafetyFilterNew
 from tbai_safe.mppi import (
   AcceleratedSafetyMPPI,
   MppiCbfCost,
@@ -240,16 +240,31 @@ def main():
   print("First message received.")
 
   rate = rospy.Rate(10)
+
+  x_desired = np.array([6.0, 6.4])
+
+  # Strict CBFs
+  factory1 = ControlBarrierFunctionFactory()
+  cbf1 = factory1.get_rectangle(method="approximate").substitute(c_x=6, c_y=2.9, w=2.0, h=2.0)
+  cbf2 = factory1.get_rectangle(method="approximate").substitute(c_x=0, c_y=2.9, w=2.0, h=2.0)
+  cbf3 = factory1.union([cbf1, cbf2], method="approximate").substitute(kappa=10)
+
+  # Soft CBFs
+  factory2 = ControlBarrierFunctionFactory()
+  cbf4 = factory2.get_sphere().substitute(c_x=4, c_y=3.4, r=1.3)
+  cbf5 = factory2.get_sphere().substitute(c_x=2, c_y=3.6, r=1.3)
+
   fig, ax = plt.subplots()
   ax.set_xlabel("X Position")
   ax.set_ylabel("Y Position")
   ax.set_title("Base X-Y Position")
-  ax.set_xlim(-2, 2)
-  ax.set_ylim(-2, 2)
+  ax.set_xlim(-1, 8)
+  ax.set_ylim(-1, 8)
   ax.legend()
   plt.show(block=False)
 
-  last_angle_diff = None
+  colors = ["red", "green", "blue"]
+  pcm = visualize_cbfs([cbf3, cbf4, cbf5], ax, granularity=200, unsafe_colors=colors, alpha=0.5)
 
   # Initialize system
   system = SimpleSingleIntegrator2D().reset(
@@ -258,64 +273,41 @@ def main():
   system.visualize()
   dt = 0.02
 
-  factory2 = ControlBarrierFunctionFactory()
-
-  d = 1.8
-  cbf10 = factory2.get_sphere().substitute(c_x=0.5, c_y=-d, r=1.3)
-  cbf11 = factory2.get_sphere().substitute(c_x=-0.5, c_y=d, r=1.3)
-  cbf12 = factory2.get_sphere().substitute(c_x=-1.6, c_y=0, r=1.3)
-  cbf10_jit = jit_expr(cbf10.get_expr(substitute=True))
-  cbf11_jit = jit_expr(cbf11.get_expr(substitute=True))
-  cbf12_jit = jit_expr(cbf12.get_expr(substitute=True))
-  pcm = visualize_cbfs([cbf10, cbf11, cbf12], ax, granularity=200, unsafe_colors=["red" for _ in range(3)], alpha=0.9)
-
-  cbf_params = [
-    {"cx": 0.5, "cy": -d, "r": 1.3, "id": 0},
-    {"cx": -0.5, "cy": d, "r": 1.3, "id": 1},
-    {"cx": -1.6, "cy": 0.0, "r": 1.3, "id": 2},
-  ]
-
-  # Publish CBF markers in RViz
-  safe_node.publish_cbf_markers(cbf_params, z=safe_node.z, height=0.05)
-
   # Get LQR controller
   Q, R = np.eye(2), np.eye(2)
 
+  # Get safety filter
+  safety_filter = VanillaSafetyFilterNew(system.get_A(), system.get_B(), cbf3, alpha=5.5)
+
   # Prepare stage cost
-  x1, x2, u1, u2 = factory2.x, factory2.y, factory2.u1, factory2.u2
-  x_desireds, y_desireds = sp.symbols("x_desired, y_desired")
-  lqr_stage_cost_expr = system.get_lqr_cost_expr(Q, R, x1, x2, u1, u2, x_desireds, y_desireds, 0.0, 0.0)
+  x1, x2, u1, u2 = factory1.x, factory1.y, factory1.u1, factory1.u2
+  lqr_stage_cost_expr = system.get_lqr_cost_expr(Q, R, x1, x2, u1, u2, x_desired[0], x_desired[1], 0.0, 0.0)
   lqr_stage_jit = jit_expr(lqr_stage_cost_expr)
+  cbf4_jit = jit_expr(cbf4.get_expr(substitute=True))
+  cbf5_jit = jit_expr(cbf5.get_expr(substitute=True))
 
   @functools.partial(cost_fn, locals=locals(), globals=globals())
-  def stage_cost(x, y, u1, u2, weight1, weight2, weight3, weight4, alpha, x_desired, y_desired):
-    cbf10_val = cbf10_jit(x=x, y=y)
-    cbf11_val = cbf11_jit(x=x, y=y)
-    cbf12_val = cbf12_jit(x=x, y=y)
-    cost = 0.0
-    cost += (-weight1 * cbf10_val) if cbf10_val < 0 else 0
-    cost += (-weight2 * cbf11_val) if cbf11_val < 0 else 0
-    cost += (-weight3 * cbf12_val) if cbf12_val < 0 else 0
-    abs_cost = -cost if cost < 0 else cost
-    if abs_cost == 0:
-      abs_cost = 1.0
-    abs_cost = max(abs_cost, 1.0)
-    cost += (alpha * lqr_stage_jit(x=x, y=y, u1=0.0, u2=0.0, x_desired=x_desired, y_desired=y_desired)) / abs_cost
+  def stage_cost(x, y, u1, u2, weight1, weight2, alpha):
+    cost = alpha * lqr_stage_jit(y=y, x=x, u1=u1, u2=u2)
+
+    cbf4_val = cbf4_jit(x=x, y=y)
+    cbf5_val = cbf5_jit(x=x, y=y)
+    cost += (-weight1 * cbf4_val) if cbf4_val < 0 else 0
+    cost += (-weight2 * cbf5_val) if cbf5_val < 0 else 0
     return cost
 
-  lqr_final_cost_expr = system.get_lqr_cost_expr(Q, R, x1, x2, u1, u2, x_desireds, y_desireds, 0.0, 0.0)
+  lqr_final_cost_expr = system.get_lqr_cost_expr(Q, R, x1, x2, u1, u2, x_desired[0], x_desired[1], 0.0, 0.0)
   lqr_final_jit = jit_expr(lqr_final_cost_expr)
 
   @functools.partial(cost_fn, locals=locals(), globals=globals())
-  def terminal_cost(x, y, weight1, weight2, weight3, weight4, alpha, x_desired, y_desired):
-    return lqr_final_jit(x=x, y=y, u1=0.0, u2=0.0, x_desired=x_desired, y_desired=y_desired)
+  def terminal_cost(x, y, weight1=1.0, weight2=1.0, alpha=1.0):
+    return lqr_final_jit(x=x, y=y, u1=0.0, u2=0.0)
 
   mppi_cost_fn = get_cost_function_parameterized(
     stage_cost,
     terminal_cost,
-    scalar_args=["weight1", "weight2", "weight3", "weight4"],
-    vector_args_ew=["alpha", "x_desired", "y_desired"],
-    vector_args_vw=[],
+    scalar_args=["weight1", "weight2"],
+    vector_args_ew=["alpha"],
   )
 
   cost = MppiCbfCost(mppi_cost_fn)
@@ -326,16 +318,25 @@ def main():
     system=system,
     lqr_Q=np.eye(2),
     lqr_R=np.eye(2),
-    dt=0.02,
-    horizon=50,
-    mc_rollouts=50000,
+    dt=0.1,
+    horizon=20,
+    mc_rollouts=1000,
     lmbda=50.0,
     sigma=np.eye(2) * 4.0,
-    transition_time=20,
-    x_desired=np.array([0, 0]),
+    transition_time=35,
+    x_desired=x_desired,
     return_sampled_trajectories=False,
     return_optimal_trajectory=True,
   )
+
+  sampled_trajectories = []
+  for _ in range(1):
+    sampled_trajectories.append(ax.plot([], [], "k-")[0])
+
+  (optimal_trajectory_plot,) = ax.plot([], [], "k-", label="Optimal trajectory")
+  weight1 = 1.0
+  weight2 = 5000.0
+  flip, publish_commands = False, False
 
   # Visualize initial and desired states
   ax.plot(safe_node.x, safe_node.y, "rx", label="Initial state", markersize=10)
@@ -354,6 +355,19 @@ def main():
   (optimal_trajectory_plot,) = ax.plot([], [], "k-", label="Optimal trajectory")
 
   last_cbf_publish_time = time.time()
+
+  def on_key_press(event):
+    nonlocal flip, publish_commands, system, fig, ax
+    if event.key == "c":
+      flip = True
+    if event.key == "p":
+      publish_commands = True
+
+  fig.canvas.mpl_connect("key_press_event", on_key_press)
+
+  # Enable grid
+  ax.minorticks_on()
+  ax.grid(which="both", alpha=0.2)
 
   while not rospy.is_shutdown():
     system.set_state(np.array([safe_node.x, safe_node.y]))
@@ -390,11 +404,6 @@ def main():
     desired_plot.set_data([x_hist[-1]], [y_hist[-1]])
     # target_plot.set_data(x_hist, y_hist)
 
-    if time.time() - last_cbf_publish_time > 3:
-      safe_node.publish_cbf_markers(cbf_params, z=safe_node.z, height=0.05)
-      last_cbf_publish_time = time.time()
-      rospy.loginfo("Published CBF markers")
-
     # Remove previous arrow if it exists
     if arrow is not None:
       arrow.remove()
@@ -408,6 +417,16 @@ def main():
       safe_node.x, safe_node.y, dx, dy, head_width=0.08, head_length=0.12, fc="g", ec="g", linewidth=2, zorder=5
     )
 
+    if flip:
+      weight1, weight2 = weight2, weight1
+      mppi.reset_relaxation()
+
+      colors[1], colors[2] = colors[2], colors[1]
+
+      pcm = visualize_cbfs([cbf3, cbf4, cbf5], ax, granularity=200, unsafe_colors=colors, alpha=0.5, pcm=pcm)
+      print("Flipped")
+      flip = False
+
     t1 = time.time()
     control, _, optimal_trajectory, st = mppi.calc_control_input(
       system.state,
@@ -415,20 +434,15 @@ def main():
         (
           cost,
           MppiCbfCostInputs(
-            scalars=[5000.0, 5000.0, 5000.0, 5000.0],
-            vectors_ew=[
-              mppi.relaxation_alphas,
-              [float(x_hist[-1]) for _ in range(mppi.T)],
-              [float(y_hist[-1]) for _ in range(mppi.T)],
-            ],
-            vectors_vw=[],
+            scalars=[weight1, weight2], vectors_ew=[np.array(mppi.relaxation_alphas[: mppi.T])], vectors_vw=[]
           ),
         ),
       ],
-      x_desired=np.array([x_hist[-1], y_hist[-1]]),
     )
-    desired_plot.set_data([x_hist[-1]], [y_hist[-1]])
+    control = safety_filter.solve(state=system.state, u_nominal=control)
     t2 = time.time()
+
+    print(f"Control: {control}")
 
     vx_desired, vy_desired = control[0], control[1]
     vx_desired_b, vy_desired_b = vx_desired * cy + vy_desired * sy, -vx_desired * sy + vy_desired * cy
@@ -436,8 +450,11 @@ def main():
     desired_yaw = np.arctan2(control[1], control[0])
 
     command_twist = Twist()
-    command_twist.linear.x = vx_desired_b * 0.999
-    command_twist.linear.y = vy_desired_b * 0.999
+    vel = np.array([vx_desired_b, vy_desired_b])
+    if np.linalg.norm(vel) > 0.5:
+      vel = vel / np.linalg.norm(vel) * 0.5
+    command_twist.linear.x = vel[0]
+    command_twist.linear.y = vel[1]
     follow = np.linalg.norm([vx_desired_b, vy_desired_b]) >= 0.2
 
     angle_diff_front = angle_diff(yaw, desired_yaw)
@@ -449,9 +466,10 @@ def main():
     else:
       chosen_angle_diff = angle_diff_back
 
-    follow = 0
-    command_twist.angular.z = np.clip(0.3 * (chosen_angle_diff * follow) + (1 - follow) * wz, -0.6, 0.6)
-    safe_node.twist_publisher.publish(command_twist)
+    command_twist.angular.z = np.clip(0.5 * (chosen_angle_diff * follow) + (1 - follow) * wz, -0.6, 0.6)
+    print(f"Command twist: {command_twist}")
+    if publish_commands:
+      safe_node.twist_publisher.publish(command_twist)
 
     if mppi.return_optimal_trajectory:
       optimal_trajectory_plot.set_data(optimal_trajectory[:, 0], optimal_trajectory[:, 1])
