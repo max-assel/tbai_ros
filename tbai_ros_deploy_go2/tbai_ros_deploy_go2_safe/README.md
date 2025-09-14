8930adf2288406d34a076bcf11502a019570ff50
lnotspotl <lnotspotl@gmail.com>
2025-09-14 18:31:24 -0400

    add new go2 safe deployment package

---
+++ tbai_ros_deploy_go2/tbai_ros_deploy_go2_safe/README.md
@@ -0,0 +1,14 @@
+## Go2 safe deployment
+
+
+```bash
+## Naive
+roslaunch tbai_ros_deploy_go2_safe deploy_go2_safe.launch network_interface:=enp3s0 run_rviz:=true use_mppi:=false
+
+## MPPI
+roslaunch tbai_ros_deploy_go2_safe deploy_go2_safe.launch network_interface:=enp3s0 run_rviz:=true use_mppi:=true
+```
+
+---
+
+### Showcase
---
+++ tbai_ros_deploy_go2/tbai_ros_deploy_go2_safe/src/go2_joystick_node.cpp
@@ -0,0 +1,23 @@
+#include <ros/ros.h>
+#include <tbai_ros_deploy_go2_safe/Go2Joystick.hpp>
+
+#include <tbai_core/Env.hpp>
+
+int main(int argc, char *argv[]) {
+    ros::init(argc, argv, "go2_joystick_node");
+    ros::NodeHandle nh;
+
+    tbai::reference::Go2Joystick joystick(nh);
+    joystick.InitDdsModel(tbai::getEnvAs<std::string>("TBAI_GO2_NETWORK_INTERFACE", true, "eth0"));
+    joystick.Start();
+
+    ros::Rate rate(20);
+
+    while (ros::ok()) {
+        ros::spinOnce();
+        joystick.publishTwist();
+        rate.sleep();
+    }
+
+    return EXIT_SUCCESS;
+}
